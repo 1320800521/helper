f9c4d8d42ab852039cd0d5218168fc8948a22767
hah <pengfei20140701@126.com>
2016-03-21 14:34:34 +0800

    新建文件夹归类，pod安装一些库

--- helpper/helpper/root/AppDelegate.h
+++ helpper/helpper/root/AppDelegate.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AppDelegate.h
 //  helpper
 //
 //  Created by 🐷 on 16/3/21.
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
 
 
 @end
