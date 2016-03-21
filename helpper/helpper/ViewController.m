3d27641e90e6bec3562333f1f271a82251442ef3
hah <pengfei20140701@126.com>
2016-03-21 13:59:09 +0800

    新建工程

---
+++ helpper/helpper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  helpper
+//
+//  Created by 🐷 on 16/3/21.
+//  Copyright © 2016年 🐷. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ helpper/helpperTests/helpperTests.m
@@ -0,0 +1,39 @@
+//
+//  helpperTests.m
+//  helpperTests
+//
+//  Created by 🐷 on 16/3/21.
+//  Copyright © 2016年 🐷. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface helpperTests : XCTestCase
+
+@end
+
+@implementation helpperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
